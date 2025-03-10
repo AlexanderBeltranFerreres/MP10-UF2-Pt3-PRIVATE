1d44765c193ad91c14e4d47714c2524b22f7b451
«Dgrozama» <dgrozama@iesebre.com>
2025-03-10 15:30:27 +0100

    prova

---
+++ mp10-uf2-odoo/odoo-tutorials/estate/__manifest__.py
@@ -0,0 +1,15 @@
+{
+   "name": "Estate",
+   "version": "1.0",
+   "summary": "Manage estate properties",
+   "category": "Real Estate",
+   "application": True,
+   "depends": ["base"],
+   "data": [
+      "security/ir.model.access.csv",
+      "views/estate_property_views.xml",
+      "views/estate_menus.xml",
+   ],
+   "installable": True,
+   "license": "LGPL-3",
+}
